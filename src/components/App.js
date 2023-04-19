7257221c5b4bb5490aa6a190aa654cbd9b005834
Endand <endand2898@gmail.com>
2023-04-19 10:43:06 -0400

    Updated way to get tweets
    Used Snapshot to do it in real time

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
 import ARouter from "./Router";
-import { authService } from "../fbase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setIsLoggedIn(true);
-        const uid = user.uid;
+
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
-  return init ? <ARouter isLoggedIn={isLoggedIn} /> : "Initializing...";
+  return init ? (
+    <ARouter isLoggedIn={isLoggedIn} userObj={userObj} />
+  ) : (
+    "Initializing..."
+  );
 }
 
 export default App;
