1f514dca7211d6087b0ff49978991232e6cc98cd
Endand <endand2898@gmail.com>
2023-04-19 17:18:36 -0400

    Changed display name

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { authService } from "../fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "../fbase";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .get();
+    console.log(tweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
 
   return (
     <>
